8dd4410c8dbce5db7bd566c15a1053e8b3abf21d
Razzaq <abah@fandm.edu>
2022-09-03 10:23:03 -0400

    added SDK files from firebase

---
+++ FranklinForums/FranklinForums/FranklinForumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FranklinForumsApp.swift
+//  FranklinForums
+//
+//  Created by Razzaq Bah on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FranklinForumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
